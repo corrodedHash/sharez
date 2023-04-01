cee5fd9a98f393bea45cc3f9e311a9af4b830a15
Lukas Wölfer <lukas.woelfer@rwth-aachen.de>
2023-04-01 02:00:18 +0200

    Started working on signed shares

--- src/util/ShareFormatter.ts
+++ src/util/ShareFormatter.ts
@@ -1,10 +1,53 @@
+const SIGN_ALGO = 'ECDSA'
+const SIGN_CURVE = 'P-256'
 export class ShareFormatter {
   share_id: number | undefined
   share_data: Uint8Array
+  public_key: CryptoKey | undefined
+  signature: ArrayBuffer | undefined
 
   constructor(share_id: number | undefined, share_data: Uint8Array) {
     this.share_id = share_id
     this.share_data = share_data
+  }
+  private get_signable_data(): Uint8Array {
+    return Uint8Array.from([this.share_id ?? 255, ...this.share_data])
+  }
+
+  async sign(keypair: CryptoKeyPair) {
+    if (this.share_id === undefined) throw new Error('Cannot sign share without id')
+    const privateKey = await crypto.subtle.exportKey('pkcs8', keypair.privateKey)
+
+    const key = await crypto.subtle.importKey(
+      'pkcs8',
+      privateKey,
+      { name: SIGN_ALGO, namedCurve: SIGN_CURVE },
+      true,
+      ['sign']
+    )
+    const signature = await crypto.subtle.sign(SIGN_ALGO, key, this.get_signable_data())
+    const publicKey = await crypto.subtle.exportKey('spki', keypair.publicKey)
+    this.public_key = await crypto.subtle.importKey(
+      'spki',
+      publicKey,
+      { name: SIGN_ALGO, namedCurve: SIGN_CURVE },
+      true,
+      ['verify']
+    )
+
+    this.signature = signature
+  }
+
+  async verify() {
+    if (this.public_key === undefined || this.signature === undefined) {
+      throw new Error('Cannot verify share with no public key information')
+    }
+    return await crypto.subtle.verify(
+      SIGN_ALGO,
+      this.public_key,
+      this.signature,
+      this.get_signable_data()
+    )
   }
 
   static fromString(input: string): ShareFormatter {
@@ -25,4 +68,13 @@ export class ShareFormatter {
     const output = Uint8Array.from([this.share_id, ...this.share_data])
     return '$' + btoa(String.fromCharCode(...output))
   }
+}
+
+export async function generateKeyPair(): Promise<CryptoKeyPair> {
+  const keyPair = await crypto.subtle.generateKey(
+    { name: SIGN_ALGO, namedCurve: SIGN_CURVE },
+    true,
+    ['sign', 'verify']
+  )
+  return keyPair
 }
